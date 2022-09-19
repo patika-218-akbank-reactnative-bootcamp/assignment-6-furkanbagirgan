234256e41c9588a07cc7e3b4cf165364cfb84a68
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-19 14:02:23 +0300

    edit app.js to wrap navigation with redux store

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {Provider} from 'react-redux';
 
-export default function App() {
+import Navigation from './src/navigation';
+import store from './src/redux/store';
+
+const App = () => {
+  //Here, the redux provider surround the application.
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
