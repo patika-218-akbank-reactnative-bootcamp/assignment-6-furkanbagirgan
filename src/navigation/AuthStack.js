467e45ba9e029c660e7cbcee0ab59bf7706c69ce
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-19 14:07:21 +0300

    add auth stack in case the user isn't logged in

---
+++ src/navigation/AuthStack.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useSelector} from 'react-redux';
+
+import Login from './../screens/Login';
+import Signup from './../screens/Signup';
+
+const Stack = createNativeStackNavigator();
+
+//It is the navigation structure that will be shown in case there is no entry yet.
+const AuthStack = () => {
+  //The theme information is accessed with the useSelector hook.
+  const theme = useSelector(state => state.theme.theme);
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{
+          headerStyle: {backgroundColor: theme === 'light' ? '#FFF' : '#000'},
+          headerTintColor: '#B9C0C8',
+          headerTitle: '',
+          headerTitleAlign: 'center',
+          headerShadowVisible: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
