419d692ccfb54153ebe5ce703522031c31d84e17
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-21 13:10:05 +0300

    change some check operations in authCheck

--- src/utilities/authCheck.js
+++ src/utilities/authCheck.js
@@ -2,29 +2,17 @@ import { Alert } from "react-native";
 
 //Incoming email and password are checked and return 1 if appropriate and 0 otherwise.
 export const checkLogin=(email,password)=>{
-  if(email !== ''){
-    if(email.includes('@')){
-      if(password !== ''){
-        if(password.length >= 8){
-          return 1;
-        }
-        else{
-          Alert.alert('Login Error','Password must contain at least 8 characters!');
-          return 0;
-        }
-      }
-      else{
-        Alert.alert('Login Error','Password cannot be left blank!');
-        return 0;
-      }
+  if(email.includes('@')){
+    if(password.length >= 8){
+      return 1;
     }
     else{
-      Alert.alert('Login Error','Please enter a valid email!');
+      Alert.alert('Login Error','Password must contain at least 8 characters!');
       return 0;
     }
   }
   else{
-    Alert.alert('Login Error','Email cannot be left blank!');
+    Alert.alert('Login Error','Please enter a valid email!');
     return 0;
   }
 }
@@ -34,13 +22,7 @@ export const checkSignup=(email,password,repeatPassword,userName)=>{
   const res=checkLogin(email,password);
   if(res===1){
     if(repeatPassword === password){
-      if(userName !== ''){
-        return 1;
-      }
-      else{
-        Alert.alert('Sign Up Error','User name cannot be left blank!');
-        return 0;
-      }
+      return 1;
     }
     else{
       Alert.alert('Sign Up Error','Passwords do not match!');
