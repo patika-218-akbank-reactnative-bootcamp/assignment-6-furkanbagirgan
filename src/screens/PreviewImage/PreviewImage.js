7e57a65ceb87f7ec29cb9ac84eb46b9acc71b1c0
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-23 18:00:38 +0300

    add location process in preview image screen

--- src/screens/PreviewImage/PreviewImage.js
+++ src/screens/PreviewImage/PreviewImage.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import {SafeAreaView, Image, View, Text, Alert} from 'react-native';
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { updateDoc, doc } from "firebase/firestore";
+import { updateDoc, doc, GeoPoint } from "firebase/firestore";
+import * as Location from 'expo-location';
 
 import styles from './PreviewImage.style';
 import Button from './../../components/Button/Button';
@@ -15,41 +16,52 @@ const PreviewImage = ({navigation,route}) => {
   //The picture is turned into a blob and saved to firebase storage.
   const share=async()=>{
     setLoading(true);
-    try{
-      const blob = await new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-          resolve(xhr.response);
-        };
-        xhr.onerror = function (e) {
-          console.log(e);
-          reject(new TypeError("Network request failed"));
-        };
-        xhr.responseType = "blob";
-        xhr.open("GET", image, true);
-        xhr.send(null);
-      });
-    
-      //Image saved to firebase storage.
-      const fileRef = ref(storage,user.id+'-sharedImg');
-      await uploadBytes(fileRef, blob);
-      blob.close();
-      const imagePath=await getDownloadURL(fileRef);
-      await updateDoc(doc(db,'users',user.docId), {
-        image: imagePath
-      });
-      Alert.alert('Image Share','Picture successfully shared');
-      navigation.goBack();
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status === 'granted') {
+      try{
+        //Get user location
+        const resultLoc = await Location.getCurrentPositionAsync({});
+        const currentLocation=new GeoPoint(resultLoc.coords.latitude,resultLoc.coords.longitude);
+
+        const blob = await new Promise((resolve, reject) => {
+          const xhr = new XMLHttpRequest();
+          xhr.onload = function () {
+            resolve(xhr.response);
+          };
+          xhr.onerror = function (e) {
+            console.log(e);
+            reject(new TypeError("Network request failed"));
+          };
+          xhr.responseType = "blob";
+          xhr.open("GET", image, true);
+          xhr.send(null);
+        });
+      
+        //Image saved to firebase storage.
+        const fileRef = ref(storage,user.id+'-sharedImg');
+        await uploadBytes(fileRef, blob);
+        blob.close();
+        const imagePath=await getDownloadURL(fileRef);
+        
+        await updateDoc(doc(db,'users',user.docId), {
+          image: imagePath,
+          location: currentLocation
+        });
+        Alert.alert('Image Share','Picture successfully shared');
+        navigation.goBack();
+      }
+      catch(error){
+        Alert.alert('Network Error','Please check your internet connection!');
+      } finally{
+        setLoading(false);
+      }
     }
-    catch(error){
-      Alert.alert('Network Error','Please check your internet connection!');
-      return '';
-    } finally{
-      setLoading(false);
+    else{
+      Alert.alert('Share Error','Pictures cannot be shared without location information.');
     }
   }
 
-  //Here, the flatlist that will appear on the screen are created.
+  //Here are the components that will appear on the screen.
   return (
     <SafeAreaView
       style={styles.container}>
