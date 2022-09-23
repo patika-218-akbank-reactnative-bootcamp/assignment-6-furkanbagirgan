74d8ee10be1075c63ebaf28801e5bdef3d7ae1b2
Furkan Bagirgan <furkanbrgn@gmail.com>
2022-09-23 17:23:28 +0300

    finish map screen

--- src/screens/Map/Map.js
+++ src/screens/Map/Map.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React,{useEffect, useState} from 'react';
+import { View,Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import {useSelector} from 'react-redux';
+import {db} from '../../utilities/firebase';
+import { query, onSnapshot, collection, where } from "firebase/firestore";
 
 import styles from './Map.style';
 
 const Map = ({navigation}) => {
   //Necessary context data and states are created.
   const theme = useSelector(state => state.theme.theme);
+  const [data,setData] = useState([]);
+
+  useEffect(()=>{
+    //User information that is the same as the current user's id is retrieved from
+    //the Firestore and saved in the user state.
+    const q = query(collection(db, "users"), where('image', '!=', ''));
+    const unsubscribe  = onSnapshot(q, (querySnapshot) => {
+      const users = [];
+      querySnapshot.forEach((doc) => {
+        users.push({id:doc.data().id,image:doc.data().image,userName:doc.data().userName,location:doc.data().location});
+      });
+      setData([...users]);
+    });
+
+    //The unsubscribe function is executed when the screen is closed.
+    return ()=>{
+      unsubscribe();
+    }
+  },[]);
+
+  //navigate to SeeImage screen
+  const seeDetail=(user)=>{
+    navigation.navigate('SeeImage',{image:user.image,userName:user.userName});
+  }
 
   //Here, the flatlist that will appear on the screen are created.
   return (
-    <SafeAreaView
-      style={styles[theme].container}>
-      <Text>Heyy</Text>
-    </SafeAreaView>
+    <MapView style={styles.container} initialRegion={{latitude:39.164139,longitude:34.093762,latitudeDelta:20,longitudeDelta:30}}>
+      {data.map((item)=>
+        <Marker key={item.id} coordinate={{latitude:item.location.latitude,longitude:item.location.longitude}} onPress={()=>seeDetail(item)}>
+          <View style={styles.imageWrapper}>
+            <Image
+              source={{uri: item.image}}
+              style={styles.image}
+            />
+          </View>
+        </Marker>
+      )}
+    </MapView>
   );
 };
 
--- src/screens/Map/Map.style.js
+++ src/screens/Map/Map.style.js
@@ -1,79 +1,23 @@
-import {Dimensions, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 
-//Here the basic styles of the Map screen are created.
-const basicStyles=StyleSheet.create({
+//Here the styles of the Map screen are created.
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 10
   },
-  errorWrapper: {
-    flex: 1,
-    flexDirection: 'column',
+  imageWrapper: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#FFFC00'
   },
-  errorText: {
-    color: 'red',
-    fontSize: 18,
-  },
-  musicContainer: {
-    width:'100%',
-  },
-  loadingContainer: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  searchBar: {
-    width: '100%',
-    height: 65,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 20,
-    paddingHorizontal: 15,
-  },
-  inputWrapper: {
-    width: '75%',
-    height: 65,
-    paddingVertical: 10,
-  },
-  buttonWrapper: {
-    width: '20%',
-    height: 45,
-    borderRadius: 15,
-    backgroundColor: '#A9A9A9',
-    alignSelf: 'flex-end',
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '90%',
+    height: '90%',
+    borderRadius: 20,
   },
 });
-
-//Here the changing styles of the Map screen are created.
-const styles = {
-  light: StyleSheet.create({
-    ...basicStyles,
-    container: {
-      ...basicStyles.container,
-      backgroundColor: 'white',
-    },
-    loadingContainer:{
-      ...basicStyles.loadingContainer,
-      backgroundColor: '#FFF'
-    }
-  }),
-  dark: StyleSheet.create({
-    ...basicStyles,
-    container: {
-      ...basicStyles.container,
-      backgroundColor: '#191414',
-    },
-    loadingContainer:{
-      ...basicStyles.loadingContainer,
-      backgroundColor: '#191414'
-    }
-  }),
-};
 
 export default styles;
